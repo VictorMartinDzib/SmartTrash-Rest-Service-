1a754f95e348a5b759ef032daf227cddfa63de14
VicMarDzib <richtzq211@gmail.com>
2020-03-29 14:45:21 -0500

    Testing server v1.0

--- __init__.py
+++
@@ -1,9 +0,0 @@
-''' author: Victor Alejandro Martin Dzib '''
-from environment import setup
-from api import app
-#from flask_pymongo import PyMongo
-
-if __name__ == '__main__':
-    app.run(debug=setup['debug'], port=setup['port'])
-
-    
---
+++ app.py
@@ -0,0 +1,17 @@
+''' author: Victor Alejandro Martin Dzib '''
+import environment
+from api import app
+from flask_pymongo import PyMongo
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Servicio para el monitoreo de basureros inteligentes'
+
+#if __name__ == '__main__':
+    #app.run()
+    #app.run(debug=environment.setup['debug'], port=environment.setup['port'])
+
+    
