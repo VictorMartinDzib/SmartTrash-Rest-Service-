7b9df986e4ed3645b130c48c1f94c1e7c95ef575
VicMarDzib <richtzq211@gmail.com>
2020-04-02 18:41:17 -0500

    add cors

--- applicaction/__init__.py
+++ applicaction/__init__.py
@@ -4,9 +4,12 @@ from applicaction.environment import setup, database
 from flask_restful import Api
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
+
 app.config["MONGO_URI"] = "mongodb://"+ database['host'] +":"+str(database['port']) +"/" + database['dbname']
 mongo = PyMongo(app)
 
